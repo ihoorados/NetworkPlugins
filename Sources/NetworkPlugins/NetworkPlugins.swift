3e1f2bebf9c7c5cb20858a79d68adfcea3f9ff7d
ihoorados <hooradramezani@gmail.com>
2022-11-28 08:45:51 +0000

    add data task interface

--- Sources/NetworkPlugins/NetworkPlugins.swift
+++ Sources/NetworkPlugins/NetworkPlugins.swift
@@ -1,6 +1,15 @@
+import Foundation
+
 public struct NetworkPlugins {
     public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
+    public init() {}
+}
+
+
+// MARK: Data Task Delegate
+protocol DataTaskDelegate: AnyObject {
+    typealias response = ((Result<Data,Error>) -> Void)
+    func startDataTask(_ request: URLRequest,completion: @escaping response)
+    func cancelDataTask()
 }
